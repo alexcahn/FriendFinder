95d14b70ce7199e52c998fbe9a1917c0f29d815f
Alex Cahn <alexbcahn@gmail>
2019-06-08 19:41:42 -0500

    htmlRoutes updated and pages working

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
-var express = require('express')
+var express = require('express');
 var path = require("path");
+var bodyParser = require("body-parser")
 
 var app = express();
 var PORT = 3000;
+
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function(req, res) {
-    // res.send("Welcome to the Star Wars Page!")
-    res.sendFile(path.join(__dirname, "app/public/home.html"));
-  });
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
-  app.get("/survey", function(req, res) {
-    // res.send("Welcome to the Star Wars Page!")
-    res.sendFile(path.join(__dirname, "app/public/survey.html"));
-  });
 
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
