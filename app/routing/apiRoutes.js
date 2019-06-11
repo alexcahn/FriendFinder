1dba365538ff721b9602358f787f5d2069f7171b
Alex Cahn <alexbcahn@gmail>
2019-06-10 19:13:59 -0500

    match correct friend working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,24 +2,52 @@ var path = require('path');
 
 var users = require('../data/friends');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-	app.get('/api/friends', function(req, res) {
-		res.json(users);
+    app.get('/api/friends', function (req, res) {
+        res.json(users);
     });
 
-    app.post('/api/friends', function(req, res) {
- 
-    var randomUser = users[Math.floor(Math.random()*users.length)];
-     res.json(randomUser)
-     console.log(randomUser)
+    app.post('/api/friends', function (req, res) {
 
-     users.push(req.body);
+        var user = req.body;
 
-     for (var i = 0; i < users.length; i++){
-         if (randomUser === users[i]){
-            continue;
-         }
-     }
+        for (var i = 0; i < user.scores.length; i++) {
+            user.scores[i] = parseInt(user.scores[i]);
+        }
+
+        var bestFriendIndex = 0;
+        var minimumDifference = 40;
+
+        for (var i = 0; i < users.length; i++) {
+            var totalDifference = 0;
+            for (var j = 0; j < users[i].scores.length; j++) {
+                var difference = Math.abs(user.scores[j] - users[i].scores[j]);
+                totalDifference += difference;
+            }
+
+            if (totalDifference < minimumDifference) {
+                bestFriendIndex = i;
+                minimumDifference = totalDifference;
+            }
+        }
+
+        users.push(user);
+
+        res.json(users[bestFriendIndex]);
     });
-}
+};
+
+    // var randomUser = users[Math.floor(Math.random()*users.length)];
+    //  res.json(randomUser)
+    //  console.log(randomUser)
+
+    //  users.push(req.body);
+
+    //  for (var i = 0; i < users.length; i++){
+    //      if (randomUser === users[i]){
+    //         continue;
+    //      }
+    //  }
+//     });
+// }
